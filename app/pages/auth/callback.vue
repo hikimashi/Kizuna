ec15b48fd9a98aeede8372d04cf236ca7d8743fc
Hikimashi <annasilvaportela@gmail.com>
2026-04-29 20:54:05 +0100

    Fix: correction d'accents

--- app/pages/auth/callback.vue
+++ app/pages/auth/callback.vue
@@ -33,7 +33,7 @@ onMounted(async () => {
       router.push('/');
     }, 2000);
   } else {
-    // Aucun code : callback invalide ou annule.
+    // Aucun code : callback invalide ou annulé.
     processed.value = true;
     success.value = false;
   }
